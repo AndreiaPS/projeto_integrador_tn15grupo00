9e02f0732e6098f4f812cd2041ec173942723e17
AndreiaFullStack <61522510+AndreiaFullStack@users.noreply.github.com>
2020-06-15 11:54:02 -0300

    Add files via upload

---
+++ modalidades.php
@@ -0,0 +1,58 @@
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?> 
+
+<head>
+<link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<header class="bg-danger text-white">
+    <div class="container text-center">
+      <h1>Ballet</h1>
+      <p class="lead">Escolha a/o professor(a): pronto, comece a praticar!</p>
+    </div>
+  </header>
+
+  <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="assets/img/ballet5.jpeg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Ballet para todas</h5>
+      <p class="card-text">Professora Fulana de tal, atua há mais de xx anos com um ballet que tras a 
+      inclusão, igualdade e auto estima. </p>
+      <a href="#" class="btn" class="saiba">Experimente</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/img/ballet4.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Ballet para todos os corpos</h5>
+      <p class="card-text">Mulheres gordas podem fazer aula de ballet sim! É uma excelente 
+      atividade física e vai te ajudar em tonificar seus musculos e manter sua saude e bem estar.</p>
+      <a href="#" class="btn" class="saiba">Experimente</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="assets/img/ballet2.jpg" alt="Imagem de capa do card">
+    <div class="card-body">
+      <h5 class="card-title">Quais as modalidades você pode fazer aqui?</h5>
+      <p class="card-text">Ballet Classico, ballet contemporâneo, ballet fitness.</p>
+      <a href="#" class="btn" class="saiba">Experimente</a>
+      
+  </div>
+  
+  <div class="card">
+      <p>Ballet é um estilo de dança que se originou nas cortes da Itália renascentista durante o século XV, e que se desenvolveu ainda mais na Inglaterra, Rússia e França como uma 
+    forma de dança de concerto.</p>
+        <ol>Beneficios do ballet</ol>
+        <li>Memoria</li>
+        <li>Bem Estar</li>
+        <li>Coordenação Motora</li>
+        <li>Tonus muscular</li>
+   </div>
+
+<div> 
+<?php require_once("./inc/footer.php"); ?> 
+</div>
+
+
+
